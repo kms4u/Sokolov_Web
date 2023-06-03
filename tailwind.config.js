dd6fb0bd149a527ec7a2eb6f5847e9c346cbf3ee
kms4u <forestagent2004@gmail.com>
2023-06-03 19:17:00 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,13 @@ module.exports = {
         light: '#cfcfcf',
         // palette
         greenyellow : '#8BB604',
+
         textcol : '#525C62',
         textcollight : '#232E35',
+
         tirecol : '#656D72',
         tirecollight : '#656D72',
+        
         headcol : '#232E35',
         headcollight : '#232E35',
       },
